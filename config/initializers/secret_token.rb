d596b8a6aae1646700c0d0063f6aba9e83d3ec36
Andrew <andrew.k.kirk@gmail.com>
2012-09-27 13:15:18 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loccasions::Application.config.secret_token = '04e796b95aa91fbd52927dd0452631de1cf4f405517496453a6be0c8ea560f9e22209517b61a583903bdfdd3a420d1b97148d2dba4e1b70fa4cba5e08a12c72b'
